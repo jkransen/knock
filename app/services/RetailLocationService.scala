cff44f59dedbb8888ae0003cf0d26d47f216227a
Jeroen Kransen <jeroen@kransen.nl>
2017-03-09 11:46:02 +0100

    csv parser

--- app/services/RetailLocationService.scala
+++ app/services/RetailLocationService.scala
@@ -2,12 +2,15 @@ package services
 
 import com.opencsv.CSVParser
 
+import scala.io.Source
+
 class RetailLocationService {
 
   val csvParser = new CSVParser()
 
   def loadRetailLocations(): Unit = {
 
-    val head = csvParser.parseLine("")
+    val lines = Source.fromFile("app/RetailLocationsList").getLines
+    val head = csvParser.parseLine(lines.head)
   }
 }
