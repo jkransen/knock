1b44938f5363516f529ba12d294a54c946078ff4
Hans Grimm <hans@grimm.nl>
2017-03-09 14:12:43 +0100

    login page

---
+++ public/js/main-min.js
@@ -0,0 +1,1 @@
+$(function(){$(".js-dropdown").click(function(){var i=$(this);i.closest("li").find("js-dropcontainer").slideToggle(500)})});
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+
+  $('.js-dropdown').click( function () {
+    var $this = $(this);
+    $this.closest('li').find('js-dropcontainer').slideToggle(500);
+  });
+
+});
