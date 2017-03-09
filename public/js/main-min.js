5dba79f645140d1a3eb78e3eb5e2b7a9f560e0ea
Hans Grimm <hans@grimm.nl>
2017-03-09 15:26:35 +0100

    login page

--- public/js/main-min.js
+++ public/js/main-min.js
@@ -1,1 +1,1 @@
-$(function(){$(".js-dropdown").click(function(){var i=$(this);i.closest("li").find("js-dropcontainer").slideToggle(500)})});
+$(function(){$(".delivery-address--more-content").hide(),$(".js-dropdown").click(function(){var e=$(this);e.closest("li").siblings().find(".delivery-address--more-content").slideUp(),e.closest("li").find(".delivery-address--more-content").slideToggle(500)}),$(".modal").hide(),$(".delivery-toolbar__notin button").click(function(){$(".modal").show()})});
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,20 @@
 $(function() {
 
-  //window.alert('yes');
+  // show/hide more content in delivery-list
+
+  $('.delivery-address--more-content').hide();
 
   $('.js-dropdown').click( function () {
     var $this = $(this);
-    $this.closest('li').find('js-dropcontainer').slideToggle(500);
+    $this.closest('li').siblings().find('.delivery-address--more-content').slideUp();
+    $this.closest('li').find('.delivery-address--more-content').slideToggle(500);
+  });
+
+
+  $('.modal').hide();
+
+  $('.delivery-toolbar__notin button').click ( function () {
+    $('.modal').show();
   });
 
 });
