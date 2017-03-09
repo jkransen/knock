69e476ab0a7132ca021696d4627fef0b376c98ab
Jeroen Kransen <jeroen@kransen.nl>
2017-03-09 14:46:31 +0100

    pagina's naar scala

--- app/controllers/RetailController.scala
+++ app/controllers/RetailController.scala
@@ -29,6 +29,7 @@ class RetailController @Inject() (retailLocationService: RetailLocationService) 
 
   def login(lat: Double, lon: Double) = Action {
     val nearestLocations = retailLocationService.getNearestLocations(lat, lon)
-    Ok(s"nearby $lat $lon: $nearestLocations")
+    //Ok(s"nearby $lat $lon: $nearestLocations")
+    Ok(views.html.delivery_list("Your new application is ready."))
   }
 }
